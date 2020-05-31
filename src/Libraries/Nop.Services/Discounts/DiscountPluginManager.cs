0beb87555e6ca1f976cdf5f17275b69bfd978d33
Kush Singh <kush.singh56@outlook.com>
2020-06-01 04:03:51 +0530

    Noop Initial Commit

---
+++ src/Libraries/Nop.Services/Discounts/DiscountPluginManager.cs
@@ -0,0 +1,22 @@
+﻿using Nop.Services.Caching;
+using Nop.Services.Customers;
+using Nop.Services.Plugins;
+
+namespace Nop.Services.Discounts
+{
+    /// <summary>
+    /// Represents a discount requirement plugin manager implementation
+    /// </summary>
+    public partial class DiscountPluginManager : PluginManager<IDiscountRequirementRule>, IDiscountPluginManager
+    {
+        #region Ctor
+
+        public DiscountPluginManager(ICacheKeyService cacheKeyService,
+            ICustomerService customerService,
+            IPluginService pluginService) : base(cacheKeyService, customerService, pluginService)
+        {
+        }
+
+        #endregion
+    }
+}
